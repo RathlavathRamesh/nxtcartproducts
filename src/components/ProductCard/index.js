489b0aa40fab0967bf9335f55d139b9e36776234
RathlavathRamesh <rameshrguktbasar@gmail.com>
2023-07-14 11:57:05 +0000

    solution

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -6,23 +6,21 @@ const ProductCard = props => {
   const {title, brand, imageUrl, rating, price, id} = productData
   return (
     //   Wrap with Link from react-router-dom
-    <Link to={`/products/${id}`}>
-      <li className="product-item">
-        <img src={imageUrl} alt="product" className="thumbnail" />
-        <h1 className="title">{title}</h1>
-        <p className="brand">by {brand}</p>
-        <div className="product-details">
-          <p className="price">Rs {price}/-</p>
-          <div className="rating-container">
-            <p className="rating">{rating}</p>
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-              alt="star"
-              className="star"
-            />
-          </div>
+    <Link to={`/products/${id}`} className="product-item">
+      <img src={imageUrl} alt="product" className="thumbnail" />
+      <h1 className="title">{title}</h1>
+      <p className="brand">by {brand}</p>
+      <div className="product-details">
+        <p className="price">Rs {price}/-</p>
+        <div className="rating-container">
+          <p className="rating">{rating}</p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+            alt="star"
+            className="star"
+          />
         </div>
-      </li>
+      </div>
     </Link>
   )
 }
